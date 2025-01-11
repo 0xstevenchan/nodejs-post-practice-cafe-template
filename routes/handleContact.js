28f6f78339969cc763b82034cebe1faa1d788152
0xstevenchan <0xstevenchan@gmail.com>
2025-01-11 13:09:32 +0800

    Add files via upload

--- routes/handleContact.js
+++ routes/handleContact.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* Your customer contact you!
-  The variable req contains name, email and message.
-  Store it to your Mongodb database (127.0.0.1:27017).
+/* Your customer has contacted you!
+   The variable req contains the name, email, and message.
+   Store it in your MongoDB database (127.0.0.1:27017).
 */
 router.post('/', function(req, res, next) {
   res.send('Hello! I am handleContact.js in "routes" folder');
